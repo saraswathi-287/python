6b40cc61859e1a5ef83360c01854028b3ec7003a
saraswathi-287 <saraswathinandini123@gmail.com>
2025-08-06 12:23:56 +0530

    Add files via upload

---
+++ amma.py
@@ -0,0 +1,16 @@
+a = lambda x,y:x+y
+print(a(10,11))
+print('SRIT')
+print('i am indented')
+print('hello')
+i=100
+print(f'{i}smallest 3 digit number')
+stock_ticker="aapl"
+price=226.41
+print('we are interested in {x} which is currently trading at {y}'.
+format(x=stock_ticker,y=price))
+thistuple = ("apple","banaa","cherry")
+if 'apple'in thistuple:
+    print("yes,'apple'is in the fruits tuple")
+a=(2)*5
+print(a)
